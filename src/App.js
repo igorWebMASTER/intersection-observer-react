9bcb06e09108aecac53e9ed204948cfcf8b6b2b4
igor.sobral <igor.sobral@pagtel.com.br>
2022-02-27 22:06:17 -0300

    feat: add lazy load with interesection observer api

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import LazyComponent from './components/Image';
+import LazyIframe from './components/Iframe';
+
+
 
 function App() {
+  const [allPokemons, setAllPokemons] = useState([]);
+  
+  useEffect(() => {
+    (async () => {
+      const { data } = await axios.get('https://pokeapi.co/api/v2/pokemon/?limit=799&offset=100');
+
+      if(data.results){
+        const pokemons = data.results.map((pokemon, index) => ({
+          name: pokemon.name,
+          id: index + 100,
+        }));
+        setAllPokemons(pokemons);
+      }
+    })()
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        {allPokemons.map(({ name, id}, index) => {
+          if(index === 25){
+            return(
+              <>
+                <LazyIframe 
+                  key='rocket'
+                  src="https://giphy.com/embed/3o7btL0Rxvh0YkpNXS"
+                  title='rocket'
+                  width="100%"
+                  height="500px"
+                /> 
+              </>
+            )
+          }
+
+          return(
+            <div key={name}>
+              <h2>{name}</h2>
+              <LazyComponent 
+                data-src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`} 
+                alt={name} 
+              />
+            </div>
+          )
+        })}
     </div>
   );
 }
